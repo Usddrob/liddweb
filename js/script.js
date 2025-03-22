8c3a18a0c62e23fad89f8ed33b7d93610afb8324
Usddrob <usddrob@gmail.com>
2025-03-21 18:37:37 -0700

    update

--- js/script.js
+++ js/script.js
@@ -188,3 +188,12 @@ if (header) {
     });
   });
 }
+// preloader
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("preloader").style.opacity = "0";
+    setTimeout(function () {
+      document.getElementById("preloader").style.display = "none";
+    }, 400); 
+  }, 1000);
+});
